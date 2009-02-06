1a5ae68df9fe332daba12174158aeef7cf438938
rajh <rajh@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-02-06 22:47:57 +0000

    Hammer sprite when healed

--- trunk/src/game/server/entities/character.cpp
+++ trunk/src/game/server/entities/character.cpp
@@ -720,6 +720,7 @@ void CHARACTER::tick()
 			player->heal_tick=server_tick();
 			if(health < 10)game.players[player->heal_from]->xp+=player->lvl*5;
 			increase_health(1);
+			game.create_hammerhit(pos);
 			game.create_sound(game.players[player->heal_from]->view_pos, SOUND_PICKUP_HEALTH, cmask_one(player->heal_from));
 		}
 		//Previous stuff (should be deleted ?)
