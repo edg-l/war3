e4db62e27e45fe002a3ce4f46c7e911db8711805
rajh <Rajh@users.noreply.github.com>
2009-02-06 21:32:44 +0000

    Stop stupid forget

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -456,7 +456,7 @@ bool PLAYER::choose_ability(int choice)
 		}
 		else if(choice==3 && lvl >=6 && !tauren_special)
 		{
-			undead_special=true;
+			tauren_special=true;
 			str_format(buf, sizeof(buf), "Invicible enabled");
 			game.send_broadcast(buf, client_id);
 			return true;
