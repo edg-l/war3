1dbf3425696657f5ccfe88113feb2b499a1203f4
rajh <Rajh@users.noreply.github.com>
2011-04-17 19:51:38 +0000

    Cleaning source code for 0.6

--- src/game/version.hpp
+++
@@ -1,7 +0,0 @@
-/* copyright (c) 2007 magnus auvinen, see licence.txt for more info */
-#include "generated/nethash.c"
-#define GAME_VERSION "0.5.1"
-#define GAME_NETVERSION "0.5 " GAME_NETVERSION_HASH
-
-//War3 version
-#define WAR3_VERSION "2.0-dev"
